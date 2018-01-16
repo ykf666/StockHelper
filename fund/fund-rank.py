3d31b3da7a8c3e331bca0fb91b1b5c81d69480ad
ykf666 <yankefeikobe@163.com>
2018-01-17 00:28:34 +0800

    fund update

--- fund/fund-rank.py
+++ fund/fund-rank.py
@@ -3,13 +3,20 @@
 
 import urllib3
 import re
+import datetime
+import json
 
+datefmt = '%Y-%m-%d'
 pool = urllib3.PoolManager()
+base_url = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz'
 
-if __name__ == "__main__":
-    url = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=' + '110026' \
-          + '&page=1&per=20&sdate=' + '2018-01-10' + '&edate=' + '2018-01-12'
-    response = pool.request('GET', url)
+
+def get_fund_price(code):
+    to_date = datetime.datetime.now()
+    from_date = to_date + datetime.timedelta(days=-10)
+    r_url = base_url + '&code=' + code + '&page=1&per=20&sdate=' + from_date.strftime(datefmt) \
+            + '&edate=' + to_date.strftime(datefmt)
+    response = pool.request('GET', r_url)
     resp_data = response.data.decode('utf-8')
     print(resp_data)
 
@@ -20,4 +27,21 @@ if __name__ == "__main__":
         match = item_re.match(line)
         if match:
             entry = match.groups()
-            print(entry[0] + ", " + entry[1])
+            break
+    return entry
+
+
+if __name__ == "__main__":
+    with open("../config/fund.json", 'r') as f:
+        conf = json.load(f)
+    if 'oBBGPwGoZ4mM0u4oP_jkXKvdTtYc' in conf:
+        for u_conf in conf['oBBGPwGoZ4mM0u4oP_jkXKvdTtYc']:
+            code = u_conf['code']
+            cost_price = u_conf['cost_price']
+            count = u_conf['count']
+            name = u_conf['name']
+            res = get_fund_price(code)
+            print(res)
+            current_price = float(res[1])
+            ratio = float('%.4f' % ((current_price - cost_price) / cost_price))
+            print(ratio)
